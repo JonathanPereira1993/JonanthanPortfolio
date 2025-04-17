6d8c6e08b862fb98fd03801f9e6b3dbbe4e8c798
Jonathan Pereira <jonathanbrancopereira@gmail.com>
2025-04-17 09:07:07 +0100

    feat: Responsive the contact page and added animation in message confirmation with joke phrases with time

--- src/pages/contacts/EmailSentAnim.tsx
+++ src/pages/contacts/EmailSentAnim.tsx
@@ -7,6 +7,18 @@ type Props = {
 
 const EmailSentAnim = ({ show }: Props) => {
   const [stage, setStage] = useState(0);
+  const [currentMessageIndex, setCurrentMessageIndex] = useState(0);
+
+  const idleMessages = [
+    "Do you like this message?",
+    "Still here? I'm flattered!",
+    "Are you thinking of closing me, or will we be looking at each other forever?",
+    "I'm not going anywhere until you tap 😄",
+    "Okay, seriously... tap me anytime now.",
+    "Okay, lets get nerd. Do you know that one day on Venus is longer than its year? Cool right?",
+    "The universe is still expanding. Like this message in relation with your time :D.",
+    "I like you very much, don't go please, Jonathan made me be hidden for the entire time! But you send him a message and now I'm free! Thank you!!!",
+  ];
 
   useEffect(() => {
     if (!show) {
@@ -15,8 +27,8 @@ const EmailSentAnim = ({ show }: Props) => {
     }
 
     let growTimeout: ReturnType<typeof setTimeout>;
-    let showMessageTimeout: ReturnType<typeof setTimeout>;
-    let exitTimeout: ReturnType<typeof setTimeout>;
+    let timePassedTimeout: ReturnType<typeof setTimeout>;
+    let startJokesTimeout: ReturnType<typeof setTimeout>;
 
     if (show) {
       setStage(1);
@@ -25,27 +37,54 @@ const EmailSentAnim = ({ show }: Props) => {
         setStage(2);
       }, 600);
 
-      showMessageTimeout = setTimeout(() => {
+      timePassedTimeout = setTimeout(() => {
         setStage(3);
-      }, 2600);
+      }, 10000);
 
-      exitTimeout = setTimeout(() => {
-        setStage(0);
-      }, 3200);
+      startJokesTimeout = setTimeout(() => {
+        setStage(4);
+      }, 18000);
     } else {
       setStage(0);
     }
 
     return () => {
       clearTimeout(growTimeout);
-      clearTimeout(showMessageTimeout);
-      clearTimeout(exitTimeout);
+      clearTimeout(timePassedTimeout);
+      clearTimeout(startJokesTimeout);
     };
   }, [show]);
 
+  // Close Messages timeflow
+  useEffect(() => {
+    if (stage === 4) {
+      let index = 0;
+      const interval = setInterval(() => {
+        index++;
+        if (index < idleMessages.length) {
+          setCurrentMessageIndex(index);
+        } else {
+          clearInterval(interval);
+        }
+      }, 20000);
+
+      return () => {
+        clearInterval(interval);
+        setCurrentMessageIndex(0);
+      };
+    } else {
+      setCurrentMessageIndex(0);
+    }
+  }, [stage, idleMessages.length]);
+
+  const closeHandle = () => {
+    setStage(3);
+    setStage(0);
+  };
+
   return (
     <AnimatePresence>
-      {stage > 0 && stage < 4 && (
+      {stage > 0 && stage < 5 && (
         <motion.div
           className="send-mail-success__anim"
           key="container"
@@ -57,15 +96,52 @@ const EmailSentAnim = ({ show }: Props) => {
           transition={{ duration: 0.6 }}
         >
           <AnimatePresence>
-            {stage >= 2 && stage < 3 && (
+            {stage >= 2 && stage < 5 && (
               <motion.div
                 key="message"
                 initial={{ opacity: 0, scale: 0.5 }}
                 animate={{ opacity: 1, scale: 1 }}
                 exit={{ opacity: 0, scale: 0.5 }}
                 transition={{ duration: 0.5 }}
+                onClick={closeHandle}
               >
-                Your email is on the way!
+                <h2>Your message is on the way!</h2>
+                <p>
+                  Thank you for taking the time to view my portfolio. I hope you
+                  enjoyed it, and I look forward to the opportunity to create
+                  something awesome together!
+                </p>
+
+                <motion.div>
+                  <AnimatePresence>
+                    {stage >= 3 && stage < 5 && (
+                      <motion.p
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        transition={{ duration: 0.4 }}
+                        className="close-message"
+                      >
+                        You can close by tapping :)
+                      </motion.p>
+                    )}
+                  </AnimatePresence>
+
+                  <AnimatePresence mode="wait">
+                    {stage >= 4 && stage < 5 && (
+                      <motion.p
+                        key={idleMessages[currentMessageIndex]}
+                        className="joke-messages"
+                        initial={{ opacity: 0, y: 10 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        exit={{ opacity: 0, y: 10 }}
+                        transition={{ duration: 0.4 }}
+                      >
+                        {idleMessages[currentMessageIndex]}
+                      </motion.p>
+                    )}
+                  </AnimatePresence>
+                </motion.div>
               </motion.div>
             )}
           </AnimatePresence>
