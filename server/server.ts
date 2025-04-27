a9afb0f9a7756b5419f88883ab3bdf29d12a57bc
Jonathan Pereira <jonathanbrancopereira@gmail.com>
2025-04-27 14:29:31 +0100

    feat:

--- server/server.ts
+++ server/server.ts
@@ -83,5 +83,5 @@ app.post("/send-email", async (req: Request, res: Response): Promise<void> => {
   }
 });
 
-const PORT = process.env.PORT || 5001;
+const PORT = 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
